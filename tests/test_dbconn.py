a15c3790d3278b31c139130debf642ea3e223b16
Harry Enke <henke@aip.de>
2020-05-02 12:34:57 +0200

    improved check and insert

--- tests/test_dbconn.py
+++ tests/test_dbconn.py
@@ -13,32 +13,44 @@ from pyplate.db_pgsql import _exprt_scm
 from pyplate.db_pgsql import PlateDB 
 
 
+def _precord(rec):
+    print("\n")
+    for r in rec:
+        row = ','.join(['{}'.format(k)
+                            for k in r])
+        print(row)    
+    print("\n")
+
 ## main ## 
 pdb = PlateDB()
 pdb.open_connection(host=PGHOST,port=PGPORT,user=PGUSER,password=PGPASSWD,database=PGDATABASE)
 #print(dir(pdb))
-print(pdb.dbversion)
+print(pdb.dbversion,'\n')
 
 tbl='applause_dr4.archive'
 sx = '*'
-qry = ("SELECT %s FROM %s;" % (sx, tbl)) 
-nrow =  pdb.execute_query(qry) 
-print(nrow)
 
-
-
-# now try insert (only works once, may be not null for timestam,p is too restritctive?
+# try insert (only works once, may be 'not null' for timestamp is too restritctive?
 cols="archive_id,archive_name,institute,timestamp_insert,timestamp_update"
-#vals= "1003,'test_dr4','aip_test',make_timestamp(2020,5,1,1,2,23.0),make_timestamp(2020,5,1,1,2,23.1)"
-vals= "1002,'test_2dr4','aip_test',make_timestamp(2020,5,1,1,2,23.0),make_timestamp(2020,5,1,1,2,23.1)"
-qry2 = ("INSERT INTO %s (%s) VALUES(%s);" % (tbl,cols,vals)) 
-nrow =  pdb.execute_query(qry2) 
+vals= [None] * 5
+vals[0]= "1000,'test_2dr4','aip_test',make_timestamp(2020,5,1,1,2,23.0),make_timestamp(2020,5,1,1,2,23.1)"
+vals[1]= "1001,'test_1dr4','aip_test',make_timestamp(2020,5,2,1,2,23.0),make_timestamp(2020,5,1,2,2,23.1)"
+vals[2]= "1002,'test_1dr4','aip_test',make_timestamp(2020,5,2,1,2,23.0),make_timestamp(2020,5,1,2,2,23.1)"
+vals[3]= "1003,'test_dr4','aip_test',make_timestamp(2020,5,1,1,2,23.0),make_timestamp(2020,5,1,1,2,23.1)"
+vals[4]= "1004,'test_dr4','aip_test',make_timestamp(2020,5,4,1,2,23.0),Null"
+for  v in vals:
+    qry2 = ("INSERT INTO %s (%s) VALUES(%s);" % (tbl,cols,v)) 
+#    nrow =  pdb.execute_query(qry2) 
 
-print(nrow)
-
-qry3 = ("SELECT %s FROM %s where archive_id = 1003;" % (sx, tbl)) 
+#print(nrow)
+#
+qry3 = ("SELECT %s FROM %s where archive_id > 1000;" % (sx, tbl)) 
 nrow =  pdb.execute_query(qry3)
 print(nrow)
+rec = pdb.cursor.fetchall()
+if(rec):
+    _precord(rec) 
+
 
 pdb.close_connection()
 
