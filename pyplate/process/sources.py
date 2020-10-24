7d196697ff02b7f487eb66dfb2ca0e58d44d63aa
Taavi Tuvikene <taavi.tuvikene@ut.ee>
2020-10-24 21:47:55 +0300

    Improved log messages in SourceTable.crossmatch_gaia

--- pyplate/process/sources.py
+++ pyplate/process/sources.py
@@ -620,9 +620,10 @@ class SourceTable(Table):
         #    tolerance = ((2 * u.arcsec / mean_pixscale)
         #                 .to(u.pixel).value)
 
-        self.log.write('Crossmatching source coordinates with reference-star '
-                       'coordinates the tolerance of {:.2f} pixels'
-                       .format(tolerance), level=4, event=44)
+        tolerance_arcsec = (5. * mean_scamp_sigma).to(u.arcsec).value
+        self.log.write('Crossmatch tolerance: {:.2f} arcsec ({:.2f} pixels)'
+                       .format(tolerance_arcsec, tolerance), level=4, event=44)
+
         ind_plate, ind_ref, ds = crossmatch_cartesian(coords_plate, xy_ref, 
                                                       tolerance=tolerance)
         dist_arcsec = (ds * u.pixel * mean_pixscale).to(u.arcsec).value
@@ -657,9 +658,12 @@ class SourceTable(Table):
         if (20. * mean_scamp_sigma) < 5 * u.arcsec:
             max_distance = (5 * u.arcsec / mean_pixscale).to(u.pixel).value
 
-        self.log.write('Find all neighbors for sources within the radius of '
-                       '{:.2f} pixels)'.format(max_distance),
+        max_dist_arcsec = (max_distance * u.pixel * mean_pixscale).to(u.arcsec).value
+        self.log.write('Finding all reference stars around sources within '
+                       'the radius of {:.2f} arcsec ({:.2f} pixels)'
+                       .format(max_dist_arcsec, max_distance),
                        level=4, event=44)
+
         mtrx = kdt_plate.sparse_distance_matrix(kdt_ref, max_distance)
         mtrx_keys = np.array([a for a in mtrx.keys()])
 
